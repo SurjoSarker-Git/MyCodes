eb2c8c17b4d4eb4ed74be0508a5127ca6199dbc1
PromitPallab831011 <promitpa;lab273@gmail.com>
2025-05-31 01:46:49 +0600

    Pushing

---
+++ 2-3moves.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        long long int n;
+
+        cin>>n;
+        int cnt1=0,cnt2=0,cnt3=0,cnt4=0;
+
+        int sum=0;
+        
+    }
+}
